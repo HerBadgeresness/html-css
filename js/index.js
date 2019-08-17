e8b98cdfab5226f71b92472e1e5d4424409fb472
Marko Martonosi <marko.martonosi@gmail.com>
2019-08-17 17:14:23 +0200

    Vezba6

--- js/index.js
+++
@@ -1,7 +0,0 @@
-//funkcija koja ocitava vrednost URL parametra pod nazivom 'name'
-var url = new URL(window.location.href);
-var userName = url.searchParams.get("name");
-if(userName === null) {
-    window.location.replace("login.html");
-}
-document.getElementById("userSpan").textContent = userName;
