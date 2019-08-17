d072ae5b45558ae951369452ddf1cb86694a1d68
HerBadgeresness <53540675+HerBadgeresness@users.noreply.github.com>
2019-08-17 17:18:29 +0200

    Remove localStorage as a datastore for todos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
+var data = {
   todo: [],
   completed: []
 };
@@ -31,7 +31,6 @@ function addItem (value) {
   document.getElementById('item').value = '';
 
   data.todo.push(value);
-  dataObjectUpdated();
 }
 
 function renderTodoList() {
@@ -48,10 +47,6 @@ function renderTodoList() {
   }
 }
 
-function dataObjectUpdated() {
-  localStorage.setItem('todoList', JSON.stringify(data));
-}
-
 function removeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
@@ -63,7 +58,6 @@ function removeItem() {
   } else {
     data.completed.splice(data.completed.indexOf(value), 1);
   }
-  dataObjectUpdated();
 
   parent.removeChild(item);
 }
@@ -81,7 +75,6 @@ function completeItem() {
     data.completed.splice(data.completed.indexOf(value), 1);
     data.todo.push(value);
   }
-  dataObjectUpdated();
 
   // Check if the item should be added to the completed list or to re-added to the todo list
   var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
